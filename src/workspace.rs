b76a833ec2b24da4f55ffdee50312bb35313c755
Seongmin Lee <boltlessengineer@gmail.com>
2023-12-24 11:48:45 +0000

    fix: change types to use as package
    - `WorkspaceManager` owns workspaces
    - derive `Debug` for `WorkspaceManager`

--- src/workspace.rs
+++ src/workspace.rs
@@ -7,9 +7,10 @@ pub struct Workspace {
     pub path: PathBuf,
 }
 
-pub struct WorkspaceManager<'a> {
-    pub workspaces: HashMap<&'a String, &'a Workspace>,
-    current_workspace: &'a String,
+#[derive(Debug)]
+pub struct WorkspaceManager {
+    pub workspaces: HashMap<String, Workspace>,
+    current_workspace: String,
 }
 
 #[derive(Debug)]
@@ -17,14 +18,15 @@ pub struct WorkspaceNotFound {
     pub workspace: String,
 }
 
-impl<'a> WorkspaceManager<'a> {
+impl<'a> WorkspaceManager {
     /// Creates a new workspace manager with a single workspace, setting it as the default.
     ///
     /// * `workspace`: The single workspace to use
-    pub fn from_single_workspace(workspace: &'a Workspace) -> WorkspaceManager<'a> {
+    pub fn from_single_workspace(workspace: Workspace) -> WorkspaceManager {
+        let name = workspace.name.clone();
         WorkspaceManager {
-            current_workspace: &workspace.name,
-            workspaces: HashMap::from([(&workspace.name, workspace)]),
+            current_workspace: name.clone(),
+            workspaces: HashMap::from([(name, workspace)]),
         }
     }
 
@@ -34,10 +36,10 @@ impl<'a> WorkspaceManager<'a> {
     /// * `workspaces`: A list of workspaces to add to the workspace manager.
     /// * `default_workspace`: The name of the default workspace.
     pub fn new(
-        workspaces: Vec<&'a Workspace>,
-        default_workspace: &'a String,
-    ) -> Result<WorkspaceManager<'a>, WorkspaceNotFound> {
-        if !workspaces.iter().any(|w| &w.name == default_workspace) {
+        workspaces: Vec<Workspace>,
+        default_workspace: String,
+    ) -> Result<WorkspaceManager, WorkspaceNotFound> {
+        if !workspaces.iter().any(|w| w.name == default_workspace) {
             Err(WorkspaceNotFound {
                 workspace: default_workspace.to_string(),
             })
@@ -46,7 +48,7 @@ impl<'a> WorkspaceManager<'a> {
                 current_workspace: default_workspace,
                 workspaces: workspaces
                     .into_iter()
-                    .map(|w| (&w.name, w))
+                    .map(|w| (w.name.clone(), w))
                     .collect::<HashMap<_, _>>(),
             })
         }
@@ -56,15 +58,15 @@ impl<'a> WorkspaceManager<'a> {
     ///
     /// * `name`: The name of the workspace.
     pub fn get_workspace(&self, name: &String) -> Option<&Workspace> {
-        self.workspaces.get(name).cloned()
+        self.workspaces.get(name)
     }
 
     /// Sets the current workspace to the workspace with the given name.
     /// Returns unit if the workspace was set, else returns a WorkspaceNotFound error.
     ///
     /// * `name`: The name of the workspace to set as the current workspace.
-    pub fn set_current_workspace(&mut self, name: &'a String) -> Result<(), WorkspaceNotFound> {
-        if self.workspaces.get(name).is_none() {
+    pub fn set_current_workspace(&mut self, name: String) -> Result<(), WorkspaceNotFound> {
+        if self.workspaces.get(&name).is_none() {
             Err(WorkspaceNotFound {
                 workspace: name.to_string(),
             })
@@ -75,16 +77,16 @@ impl<'a> WorkspaceManager<'a> {
     }
 
     /// Returns the current workspace.
-    pub fn get_current_workspace(&self) -> &'a Workspace {
-        self.workspaces.get(self.current_workspace).unwrap()
+    pub fn get_current_workspace(&self) -> &Workspace {
+        self.workspaces.get(&self.current_workspace).unwrap()
     }
 
     /// Adds a workspace to the list of workspaces.
     /// Overwrites any existing workspace with the same name.
     ///
     /// * `workspace`: The workspace to add to the list of workspaces.
-    pub fn add_workspace(&mut self, workspace: &'a Workspace) {
-        self.workspaces.insert(&workspace.name, workspace);
+    pub fn add_workspace(&mut self, workspace: Workspace) {
+        self.workspaces.insert(workspace.name.clone(), workspace);
     }
 }
 
@@ -99,8 +101,8 @@ mod tests {
             path: "~/some/path".into(),
         };
 
-        let workspace_manager = WorkspaceManager::from_single_workspace(&workspace);
-        assert_eq!(workspace_manager.current_workspace, &workspace.name);
+        let workspace_manager = WorkspaceManager::from_single_workspace(workspace);
+        assert_eq!(&workspace_manager.current_workspace, "example name");
     }
 
     #[test]
@@ -116,8 +118,9 @@ mod tests {
         };
 
         let workspace_manager =
-            WorkspaceManager::new(vec![&workspace1, &workspace2], &workspace1.name).expect("");
-        assert_eq!(workspace_manager.current_workspace, &workspace1.name);
+            WorkspaceManager::new(vec![workspace1, workspace2], "example name".to_string())
+                .unwrap();
+        assert_eq!(&workspace_manager.current_workspace, "example name");
     }
 
     #[test]
@@ -133,15 +136,15 @@ mod tests {
         };
 
         let mut workspace_manager =
-            WorkspaceManager::new(vec![&workspace1, &workspace2], &workspace1.name)
+            WorkspaceManager::new(vec![workspace1, workspace2], "example name".to_string())
                 .expect("Workspace 1 not found!");
         workspace_manager
-            .set_current_workspace(&workspace2.name)
+            .set_current_workspace("another example name".to_string())
             .expect("Unable to set the current workspace");
 
         assert_eq!(
             &workspace_manager.get_current_workspace().name,
-            &workspace2.name
+            "another example name"
         );
     }
 }
